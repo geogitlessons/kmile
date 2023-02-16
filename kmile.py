896f32afc2d4742b37cffceb27a0518eeb29cc9e
Misha Kantaria <test@test.com>
2023-02-16 21:06:33 +0400

    Add convertation.

--- kmile.py
+++ kmile.py
@@ -6,4 +6,14 @@ unit = input("km or mile: ")
 amount = float(input("Amount: "))
 
 
+if unit == "km":
+  print(amount / 1.609)
+
+elif unit == "mile":
+  print(amount * 1.609)
+
+else:
+  print("Error")
+
+
 
